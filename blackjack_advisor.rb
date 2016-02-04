eeaf3e3e09f5122555e2e9349ab11e610ac8aedf
Molly G <gehring.mg@gmail.com>
2016-02-03 23:29:54 -0500

    Simplify more hashes with method

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,7 +1,7 @@
 def total(card1, card2)
   card1.to_i + card2.to_i
 end
-#
+
 def aces_to_eleven(card)
   if card == "A" || card == "a"
     return true
@@ -14,25 +14,15 @@ def face_cards(card)
   end
 end
 
-# def hand_type(card1, card2)
-#   type = 0
-#   if card1 == card2
-#     type = pair_hash
-#   elsif card1 == 11 || card2 == 11
-#     type = soft_hash
-#   else
-#     type = hard_hash
-#   end
-#   return type
-# end
-
-
 sub_hash_h = Hash.new("Hit")
 simplified_h = Hash.new("Hit")
 (2..6).each do |x|
   simplified_h[x] = "Stand"
 end
-#sub_hash_hb = {5 => "Double if possible, otherwise hit", 6 => "Double if possible, otherwise hit"}
+simplified_h_2 = Hash.new("Hit")
+(4..6).each do |x|
+  simplified_h_2[x] = "Double if possible, otherwise hit"
+end
 sub_hash_dh = Hash.new("Double if possible, otherwise hit")
 sub_hash_s = Hash.new("Stand")
 sub_hash_p = Hash.new("Split")
@@ -56,10 +46,10 @@ hard_hash = { 5 => sub_hash_h,
               21 => sub_hash_s
 }
 
-soft_hash = { 13 => sub_hash_h.merge({4 => "Double if possible, otherwise hit", 5 => "Double if possible, otherwise hit", 6 => "Double if possible, otherwise hit"}),
-              14 => sub_hash_h.merge({4 => "Double if possible, otherwise hit", 5 => "Double if possible, otherwise hit", 6 => "Double if possible, otherwise hit"}),
-              15 => sub_hash_h.merge({4 => "Double if possible, otherwise hit", 5 => "Double if possible, otherwise hit", 6 => "Double if possible, otherwise hit"}),
-              16 => sub_hash_h.merge({4 => "Double if possible, otherwise hit", 5 => "Double if possible, otherwise hit", 6 => "Double if possible, otherwise hit"}),
+soft_hash = { 13 => simplified_h_2,
+              14 => simplified_h_2,
+              15 => simplified_h_2,
+              16 => simplified_h_2,
               17 => sub_hash_h.merge({2 => "Double if possible, otherwise hit", 3 => "Double if possible, otherwise hit", 4 => "Double if possible, otherwise hit", 5 => "Double if possible, otherwise hit", 6 => "Double if possible, otherwise hit"}),
               18 => sub_hash_s.merge({3 => "Double if possible, otherwise stand", 4 => "Double if possible, otherwise stand", 5 => "Double if possible, otherwise stand", 6 => "Double if possible, otherwise stand", 9 => "Hit", 10 => "Hit"}),
               19 => sub_hash_s.merge({6 => "Double if possible, otherwise stand"}),
